2c39af66dc11e08098ea18f825c591fb88e463c0
Maximilian Danelli <maxdanelli@gmail.com>
2019-09-14 18:42:09 +0200

    found errors in implementation; todo added

--- ndfs/src/ndfs/mcndfs_1_naive/NNDFS.java
+++ ndfs/src/ndfs/mcndfs_1_naive/NNDFS.java
@@ -11,16 +11,16 @@ import ndfs.NDFS;
 
 /**
  * Implements the {@link ndfs.NDFS} interface, mostly delegating the work to a
- * worker class. max was here
+ * worker class. 
  */
 public class NNDFS implements NDFS {
 
     private final Worker worker;
 
     // map of red states shared between threads
-    private ConcurrentHashMap<State, Boolean> red_states = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<State, Boolean> red_states = new ConcurrentHashMap<>();
     // number of threads that called dfsRed() on the state; also shared
-    private ConcurrentHashMap<State, AtomicInteger> thread_count = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<State, AtomicInteger> thread_count = new ConcurrentHashMap<>();
 
     /**
      * Constructs an NDFS object using the specified Promela file.
@@ -28,8 +28,9 @@ public class NNDFS implements NDFS {
      * @param promelaFile the Promela file.
      * @throws FileNotFoundException is thrown in case the file could not be read.
      */
-    public NNDFS(File promelaFile) throws FileNotFoundException {
+    public NNDFS(File promelaFile, int num_worker) throws FileNotFoundException {
 
+        /**TODO initalization is incorrect, we need to map the states; keyset is empty right now */
         red_states.keySet().forEach(x -> red_states.put(x, false)); // no state is red at start
         thread_count.keySet().forEach(x -> thread_count.put(x, new AtomicInteger(0))); // counters start at 0
         this.worker = new Worker(promelaFile, red_states, thread_count);
