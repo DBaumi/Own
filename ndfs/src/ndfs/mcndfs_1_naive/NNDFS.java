eb77a86164e9a875125a5c9b95dc5150c0f50dbf
Maximilian Danelli <maxdanelli@gmail.com>
2019-09-17 17:59:14 +0200

    graph dfs traversal and map inititialization
    algorithm wont terminate

--- ndfs/src/ndfs/mcndfs_1_naive/NNDFS.java
+++ ndfs/src/ndfs/mcndfs_1_naive/NNDFS.java
@@ -1,26 +1,29 @@
 package ndfs.mcndfs_1_naive;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Stack;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import graph.State;
+import graph.*;
 
 import ndfs.NDFS;
 
 /**
  * Implements the {@link ndfs.NDFS} interface, mostly delegating the work to a
- * worker class. 
+ * worker class.
  */
 public class NNDFS implements NDFS {
 
     private final Worker worker;
 
     // map of red states shared between threads
-    public ConcurrentHashMap<State, Boolean> red_states = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<graph.State, Boolean> red_states = new ConcurrentHashMap<>();
     // number of threads that called dfsRed() on the state; also shared
-    public ConcurrentHashMap<State, AtomicInteger> thread_count = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<graph.State, AtomicInteger> thread_count = new ConcurrentHashMap<>();
 
     /**
      * Constructs an NDFS object using the specified Promela file.
@@ -30,9 +33,23 @@ public class NNDFS implements NDFS {
      */
     public NNDFS(File promelaFile, int num_worker) throws FileNotFoundException {
 
-        /**TODO initalization is incorrect, we need to map the states; keyset is empty right now */
-        red_states.keySet().forEach(x -> red_states.put(x, false)); // no state is red at start
-        thread_count.keySet().forEach(x -> thread_count.put(x, new AtomicInteger(0))); // counters start at 0
+        /**init hashmaps with default values by iterating over the graph with a stack */
+        Graph myGraph = GraphFactory.createGraph(promelaFile);
+        State s = myGraph.getInitialState();
+        Stack<State> stack = new Stack<State>();
+        stack.push(s);
+        while (!stack.empty())
+        {
+            State current = stack.pop();
+            red_states.put(current, false);
+            thread_count.put(current, new AtomicInteger(0));
+            List<graph.State> next_states = myGraph.post(current);
+            for (State next : next_states)
+            {
+                stack.push(next);
+            }
+        }
+
         this.worker = new Worker(promelaFile, red_states, thread_count);
     }
 
